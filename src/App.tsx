7f6e93dbc66fae1e00f9c2a864439bfc70183de4
Marlee Garcia <mgarcia@onemagnify.com>
2023-07-07 12:10:47 -0400

    Going backwards woooh

--- src/App.tsx
+++ src/App.tsx
@@ -5,46 +5,44 @@ import { Link, Route, Routes } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import Home from "./pages/Home";
 import FrequentlyAsked from "./pages/FrequentlyAsked";
-import Contact from "./pages/Contact";
 
 function App() {
-  return (
-    <div className="App bg-phone bg-cover min-h-screen overflow-y-hidden overflow-x-hidden flex flex-col w-full">
-      <header className="flex fixed shadow-md pr-2 justify-end items-center font-text text-xl bg-neutral-800 bg-opacity-30 w-full h-11">
-        <Link
-          to="/"
-          className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-        >
-          Home
-        </Link>
-        <Link
-          to="/portfolio"
-          className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-        >
-          Portfolio
-        </Link>
-        <Link
-          to="/FrequentlyAsked"
-          className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-        >
-          FAQ
-        </Link>
-        <Link
-          to="/contact"
-          className="text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-        >
-          Booking
-        </Link>
-      </header>
+	return (
+		<div className="bg-phone bg-cover min-h-screen overflow-y-hidden overflow-x-hidden flex flex-col w-full">
+			<header className="flex fixed shadow-md pr-2 justify-end items-center font-text text-xl bg-neutral-800 bg-opacity-30 w-full h-11">
+				<Link
+					to="/"
+					className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+				>
+					Home
+				</Link>
+				<Link
+					to="/portfolio"
+					className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+				>
+					Portfolio
+				</Link>
+				<Link
+					to="/FrequentlyAsked"
+					className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+				>
+					FAQ
+				</Link>
+				<Link
+					to="/contact"
+					className="text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+				>
+					Booking
+				</Link>
+			</header>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/FrequentlyAsked" element={<FrequentlyAsked />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </div>
-  );
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/portfolio" element={<Portfolio />} />
+				<Route path="/FrequentlyAsked" element={<FrequentlyAsked />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
