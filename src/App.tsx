2993430f9f26f1a0ecd85c514bba1cd2b6fe07b6
Marlee Garcia <mgarcia@onemagnify.com>
2023-07-10 11:28:16 -0400

    Updated homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,48 @@ import { Link, Route, Routes } from "react-router-dom";
 import Portfolio from "./pages/Portfolio";
 import Home from "./pages/Home";
 import FrequentlyAsked from "./pages/FrequentlyAsked";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
-	return (
-		<div className="bg-phone bg-cover min-h-screen overflow-y-hidden overflow-x-hidden flex flex-col w-full">
-			<header className="flex fixed shadow-md pr-2 justify-end items-center font-text text-xl bg-neutral-800 bg-opacity-30 w-full h-11">
-				<Link
-					to="/"
-					className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-				>
-					Home
-				</Link>
-				<Link
-					to="/portfolio"
-					className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-				>
-					Portfolio
-				</Link>
-				<Link
-					to="/FrequentlyAsked"
-					className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-				>
-					FAQ
-				</Link>
-				<Link
-					to="/contact"
-					className="text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
-				>
-					Booking
-				</Link>
-			</header>
+  return (
+    <main>
+      <div>
+        {/* <header className="flex fixed shadow-md pr-2 justify-end items-center font-text text-xl bg-neutral-800 bg-opacity-30 w-full h-11">
+        <Link
+          to="/"
+          className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+        >
+          Home
+        </Link>
+        <Link
+          to="/portfolio"
+          className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+        >
+          Portfolio
+        </Link>
+        <Link
+          to="/FrequentlyAsked"
+          className="mr-2 text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+        >
+          FAQ
+        </Link>
+        <Link
+          to="/contact"
+          className="text-shadow-lg text-neutral-300 bg-opacity-0 hover:text-neutral-500"
+        >
+          Booking
+        </Link>
+      </header> */}
+      </div>
 
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/portfolio" element={<Portfolio />} />
-				<Route path="/FrequentlyAsked" element={<FrequentlyAsked />} />
-			</Routes>
-		</div>
-	);
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/FrequentlyAsked" element={<FrequentlyAsked />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {
-			backgroundImage: {
-				phone: "url('/images/moresoulbackgroundphone.png')",
-				desktop: "url('/images/moresoulbackground.png')",
-			},
-			fontFamily: {
-				title: ['"Pirata One"', "cursive"],
-				text: ['"Nunito"', "sans-serif"],
-			},
-		},
-	},
-	plugins: [require("tailwindcss-textshadow")],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        phone: "url('/images/cardboardbackground.png')",
+        desktop: "url('/images/cardboardbackground.png')",
+      },
+      fontFamily: {
+        title: ['"Asul"', "sans-serif"],
+        text: ['"Asul"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-textshadow")],
 };
